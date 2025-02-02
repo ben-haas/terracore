eb3cbef18a4cf1257132b3129adcef0a09858b62
Ben Haas <Benjamin.Haas@usda.gov>
2025-02-01 17:48:08 -0800

    add logo and message to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex min-h-screen flex-col items-center justify-center gap-8">
+	<img src="/TerraCoreLogo_black.png" alt="TerraCore Logo" class="h-auto max-w-[60%]" />
+	<h1 class="text-4xl font-bold text-black">Coming Soon</h1>
+</div>
